465406cd1b7f245fe69f72c65502436977963626
Miguel.P <meegel@gmail.com>
2025-09-01 20:15:11 -0700

    finish datatypes_operators exercise03

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,21 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int a, b;
+        a = 4;
+        b = 3;
+
+        int add = a + b;
+        int sub = a - b;
+        int mult = a * b;
+        double div = (double) a / b;
+        int mod = a % b;
+
+        System.out.println("add: " + add);
+        System.out.println("sub: " + sub);
+        System.out.println("mult: " + mult);
+        System.out.println("div: " + div);
+        System.out.println("mod: " + mod);
 
     }
 
