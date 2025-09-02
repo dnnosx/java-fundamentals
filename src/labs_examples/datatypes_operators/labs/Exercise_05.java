279dd73125fe6fa2613f6c4e2ea95fa3d1f47c1f
Miguel.P <meegel@gmail.com>
2025-09-02 15:36:53 -0700

    complete datatypes_operators exercise05

--- src/labs_examples/datatypes_operators/labs/Exercise_05.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_05.java
@@ -13,13 +13,40 @@ class LogicalOperators {
         // example of "OR"
         boolean a = true;
         boolean b = false;
-        if (a | b){
+        if (a | b) {
             System.out.println("a or b is true");
         }
 
         // write your code below
 
+        // AND
+        if (a & b) {
+            System.out.println("&: a and b must be true");
+        }
+
+        // short-circuit AND
+        if (a && b) {
+            System.out.println("&&: if a is false, b is skipped. They both need to be true");
+        }
+
+        // OR
+        if (a | b) {
+            System.out.println("\n| :  a or b is true");
+        }
+
+        // short-circuit OR
+        if (a || b) {
+            System.out.println("||:  a is true so b is skipped. Either must be true");
+        }
+
+        // XOR
+        if (a ^ b) {
+            System.out.println("^ :  a or b must be true but not both");
+        }
+
+        // NOT
+        if (!a) {
+            System.out.println("!: only if a is NOT true this will print");
+        }
     }
-
 }
-
