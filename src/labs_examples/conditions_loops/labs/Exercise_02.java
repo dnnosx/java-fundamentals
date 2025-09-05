82ac5b563fe307bc838932fc25bd0638d17af2f4
Miguel.P <meegel@gmail.com>
2025-09-05 11:09:01 -0700

    complete conditions_loops lab02

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -13,9 +13,36 @@ package labs_examples.conditions_loops.labs;
 
 public class Exercise_02 {
 
-    public static void main(String[] args) {
-    
+    public static void main(String[] args)
+            throws java.io.IOException {
         // write completed here
 
+        System.out.print("What day? ");
+        int number = (char)System.in.read();
+
+        if (number == '1'){
+            System.out.println("Sunday");
+        }
+        else if (number == '2') {
+            System.out.println("Monday");
+        }
+        else if (number == '3') {
+            System.out.println("Tuesday");
+        }
+        else if (number == '4') {
+            System.out.println("Wednesday");
+        }
+        else if (number == '5') {
+            System.out.println("Thursday");
+        }
+        else if (number == '6') {
+            System.out.println("Friday");
+        }
+        else if (number == '7') {
+            System.out.println("Saturday");
+        }
+        else {
+            System.out.println("I am error.");
+        }
     }
 }
