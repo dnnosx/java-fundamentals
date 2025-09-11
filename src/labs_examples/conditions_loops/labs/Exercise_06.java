9101cdfe086b975cfa58ae8eacf4748d3e190cb5
Miguel.P <meegel@gmail.com>
2025-09-10 20:35:28 -0700

    complete conditions_loops lab06

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -8,4 +8,17 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_06 {
+    public static void main(String[] args) {
+
+        int counter = 0;
+        int sum = 0;
+
+        do {
+            counter++;
+            sum += counter;
+        } while (counter != 100);
+
+        System.out.println("Sum: " + sum);
+    }
+
 }
