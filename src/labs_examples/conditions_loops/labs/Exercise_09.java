c77b4fd88f38510fa02b85c4f1ba3ef68d070687
Miguel.P <meegel@gmail.com>
2025-09-11 14:23:48 -0700

    complete conditions_loops lab09

--- src/labs_examples/conditions_loops/labs/Exercise_09.java
+++ src/labs_examples/conditions_loops/labs/Exercise_09.java
@@ -8,4 +8,20 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("- " + i);
+            for (int x = 0; x < 5; x++){
+                System.out.println("-- " + x);
+                if (x == 3){
+                    break;
+                }
+            }
+
+            if (i == 3){
+                break;
+            }
+        }
+    }
 }
