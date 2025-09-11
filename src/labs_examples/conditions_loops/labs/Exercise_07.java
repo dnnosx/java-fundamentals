d87b6a7057eb79757af00c389d5e97475868cb3a
Miguel.P <meegel@gmail.com>
2025-09-11 13:31:10 -0700

    complete conditions_loops lab07

--- src/labs_examples/conditions_loops/labs/Exercise_07.java
+++ src/labs_examples/conditions_loops/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 7: First vowel
  *
@@ -14,4 +16,26 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String vowels = "aeiou";
+        int vowelsLength = vowels.length();
+
+        System.out.print("Type word: ");
+        String word = scanner.nextLine();
+        int wordLength = word.length();
+
+        for (int i = 0; i < wordLength; i++){
+            for (int x = 0; x < vowelsLength; x++){
+                if (word.charAt(i) == vowels.charAt(x) ){
+                    System.out.println(word.charAt(i));
+                    System.out.println(word);
+                    scanner.close();
+
+                    return;
+                }
+            }
+        }
+    }
 }
