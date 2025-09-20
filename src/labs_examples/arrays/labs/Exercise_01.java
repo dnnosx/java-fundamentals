5c0cfadcb2b4d74e2ddd6c1cd268be646a45e143
Miguel.P <meegel@gmail.com>
2025-09-20 15:49:41 -0700

    complete arrays lab01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,6 +1,7 @@
 package labs_examples.arrays.labs;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -13,5 +14,25 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[10];
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int number : numbers){
+            sum += number;
+        }
+
+        System.out.print("Sum of all numbers: ");
+        System.out.println(sum);
+
+        System.out.print("Average of all numbers: ");
+        System.out.println(sum / numbers.length);
+    }
 }
