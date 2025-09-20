b3a10e23ac24543c103c851f2bcd6f7b609dd09b
Miguel.P <meegel@gmail.com>
2025-09-20 16:07:27 -0700

    complete arrays lab02

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -15,6 +17,15 @@ public class Exercise_02 {
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         // write code here
+        Scanner scanner = new Scanner(System.in);
+        int index;
+
+        do {
+            System.out.print("Which index 1 - 10? ");
+            index = scanner.nextInt();
+        } while (index < 1 || index > 10);
+
+        System.out.println(array[index - 1]);
 
 
     }
