46d0f9d25d9353643b09474d5309d770f486e14e
Miguel.P <meegel@gmail.com>
2025-09-29 16:20:26 -0700

    complete arrays lab07

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +12,25 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList();
+
+        int i = 0;
+
+        nums.add(2);
+        nums.add(0, 1);
+
+        do {
+            nums.add(i * 5);
+            i++;
+        } while (nums.size() < 10);
+
+        System.out.println(nums);
+
+        int x = nums.get(1);
+        int y = nums.get(nums.size() - 1);
+        int product = x * y;
+        System.out.println(x + " * " + y + " = "  + product);
+
+    }
 }
